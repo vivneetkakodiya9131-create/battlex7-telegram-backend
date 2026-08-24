9e271e86b4488bf3745794741c98860afb00c354
vivneetkakodiya9131-create <vivneetkakodiya9131@gmail.com>
2026-08-25 03:41:57 +0530

    Add CORS support to the server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 
 const PORT = process.env.PORT || 10000;
