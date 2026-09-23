4b9ddc8457b414d1d3c659d4af2ee591492b6e6c
vivneetkakodiya9131-create <vivneetkakodiya9131@gmail.com>
2026-09-23 07:43:00 +0530

    Remove unnecessary closing brace in server.js

--- server.js
+++ server.js
@@ -16273,7 +16273,6 @@ if (firebaseReady && decoded?.uid) {
         `Aapki problem aur proof support team ko Telegram par bhej diya gaya hai.\n\n` +
         `Support team ab aapki complaint review karegi.`
     });
-  }
 
 } catch (ticketError) {
 
